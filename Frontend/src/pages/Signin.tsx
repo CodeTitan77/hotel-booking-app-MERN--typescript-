31f2a5aa1cdfe5e25847136c0073d28f7d0a28fe
Himanshu Trivedi <himanshutrivedi421@gmail.com>
2024-05-25 00:30:57 +0530

    Added Signout

--- Frontend/src/pages/Signin.tsx
+++ Frontend/src/pages/Signin.tsx
@@ -1,6 +1,8 @@
 import { useForm } from "react-hook-form"
 import { useMutation } from "react-query";
 import * as apiClient from '../api-client';
+import { useAppContext } from "../contexts/AppContext";
+import { useNavigate } from "react-router-dom";
 
 
 export type SignInFormData ={
@@ -10,13 +12,16 @@ export type SignInFormData ={
 
 }
 const SignIn= ()=>{
+    const {showToast}= useAppContext();
+    const navigate= useNavigate();
     const {register,handleSubmit, formState:{errors}}= useForm<SignInFormData>();
     const mutation = useMutation(apiClient.signIn,{
         onSuccess: async()=>{
-            console.log("user has been signed in");
+           showToast({message:"Sign in Successful",type :"SUCCESS"});
+           navigate("/");
         },
         onError : (error:Error)=>{
-            // show the toast of error
+            showToast({message:error.message,type:"ERROR"})
 
         }
     });
