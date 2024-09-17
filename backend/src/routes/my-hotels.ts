635e705d1dbbf4e398ff737ca3823160a6d713e5
Himanshu Trivedi <himanshutrivedi421@gmail.com>
2024-09-18 04:07:56 +0530

    added express validator to add- hotel form

--- backend/src/routes/my-hotels.ts
+++ backend/src/routes/my-hotels.ts
@@ -19,13 +19,27 @@ const upload = multer({
 
 router.post("/",
 verifyToken, [
- body("name").notEmpty().withMessage('Name is required'),
- body("city").notEmpty().withMessage('City is required'),
- body("country").notEmpty().withMessage('Country is required'),
- body("description").notEmpty().withMessage('Description is required'),
- body("type").notEmpty().withMessage('Hotel Type is required'),
- body("pricePerNight").notEmpty().isNumeric().withMessage("Price per night is required and must be a number"),
- body("facilities").notEmpty().isArray().withMessage('Facilities is required'),
+ body("name").
+ notEmpty().
+ withMessage('Name is required'),
+ body("city").
+ notEmpty().
+ withMessage('City is required'),
+ body("country").
+ notEmpty().
+ withMessage('Country is required'),
+ body("description").
+ notEmpty().
+ withMessage('Description is required'),
+ body("type").
+ notEmpty().withMessage('Hotel Type is required'),
+ body("pricePerNight").
+ notEmpty().
+ isNumeric().
+ withMessage("Price per night is required and must be a number"),
+ body("facilities").
+ notEmpty().isArray().
+ withMessage('Facilities is required'),
  
 
 ],
