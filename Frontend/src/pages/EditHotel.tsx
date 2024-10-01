25305c7417b6d41884f42cdd75b05d2006acd252
Himanshu Trivedi <himanshutrivedi421@gmail.com>
2024-10-02 04:00:33 +0530

    added edit feature

--- Frontend/src/pages/EditHotel.tsx
+++ Frontend/src/pages/EditHotel.tsx
@@ -1,37 +1,37 @@
-import { useParams } from "react-router-dom";
-
 import { useMutation, useQuery } from "react-query";
-import * as apiClient from "../api-client"
+import { useParams } from "react-router-dom";
+import * as apiClient from "../api-client";
 import ManageHotelForm from "../forms/ManageHotelForm/ManageHotelForm";
 import { useAppContext } from "../contexts/AppContext";
 
-
 const EditHotel = () => {
-    const {hotelId}= useParams();
-    const {showToast}= useAppContext();
-    const {data: hotel} = useQuery("fetchMyHotelById",()=>
-    apiClient.fetchMyHotelsById(hotelId||''),{
-        enabled: !!hotelId, // doble exclamation mark here query only gonna run if hotel id is present
+  const { hotelId } = useParams();
+  const { showToast } = useAppContext();
 
+  const { data: hotel } = useQuery(
+    "fetchMyHotelById",
+    () => apiClient.fetchMyHotelById(hotelId || ""),
+    {
+      enabled: !!hotelId,
     }
-);
-const {mutate,isLoading}= useMutation(apiClient.updateMyHotelById,{
-    onSuccess:()=>{
-        showToast({message:"hotel Saved",type:"SUCCESS"})
+  );
+
+  const { mutate, isLoading } = useMutation(apiClient.updateMyHotelById, {
+    onSuccess: () => {
+      showToast({ message: "Hotel Saved!", type: "SUCCESS" });
     },
-    onError:()=>{
-        showToast({message:"Error saving hotel",type:"ERROR"})
-    }
-    
-})
-const handleSave = (hotelFormData:FormData)=>{
-    mutate(hotelFormData);
-}
-  return (
+    onError: () => {
+      showToast({ message: "Error Saving Hotel", type: "ERROR" });
+    },
+  });
 
-      <ManageHotelForm hotel ={hotel} onSave={handleSave} isLoading={isLoading}/>
-   
-  )
+  const handleSave = (hotelFormData: FormData) => {
+    mutate(hotelFormData);
+  };
+
+  return (
+    <ManageHotelForm hotel={hotel} onSave={handleSave} isLoading={isLoading} />
+  );
 };
 
 export default EditHotel;
